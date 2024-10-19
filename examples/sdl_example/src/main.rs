6b3bca4aa017f3f4ee9fae177d909fc1fa9ceae1
Juanperias <136520331+Juanperias@users.noreply.github.com>
2024-10-19 16:50:10 -0400

    feat: improve the sdl example

--- examples/sdl_example/src/main.rs
+++ examples/sdl_example/src/main.rs
@@ -1,10 +1,15 @@
 extern crate sdl2;
+use record::collision::shape::Shape;
+use record::collision::{self, Collision2d};
 use record::forces::gravity::{gravity, EARTH_GRAVITY};
-use record::obj::obj_2d::obj2d;
+use record::forces::r#move::{move2d, Direction2d};
+use record::formulas::elastic_collision;
+use record::obj::obj_2d::{obj2d, Object2d};
 use record::vec::vec_2d::vec2;
 use sdl2::event::Event;
 use sdl2::gfx::primitives::DrawRenderer;
 use sdl2::pixels::Color;
+use std::collections;
 use std::time::{Duration, Instant};
 
 fn main() -> Result<(), String> {
@@ -18,12 +23,22 @@ fn main() -> Result<(), String> {
 
     let mut canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
     let mut obj = obj2d(
-        vec2(100.0, 200.0),
+        vec2(75.0, 200.0),
         2.0,
         2.0,
-        vec2(10.0, 231.0),
-        vec2(10.0, -240.0),
+        vec2(2.0, 231.0),
+        vec2(2.0, -240.0),
         20.0,
+        Shape::Circle,
+    );
+    let mut obj2 = obj2d(
+        vec2(300.0, 200.0),
+        2.0,
+        60.0,
+        vec2(0.0, 0.0),
+        vec2(0.0, 0.0),
+        20.0,
+        Shape::Circle,
     );
 
     let mut event_pump = sdl_context.event_pump()?;
@@ -43,12 +58,33 @@ fn main() -> Result<(), String> {
 
         canvas.set_draw_color(Color::RGB(255, 255, 255));
         canvas.clear();
-        obj.apply(gravity(EARTH_GRAVITY, dt, dt));
+        //obj.apply(gravity(EARTH_GRAVITY, dt, dt));
+        obj.apply(move2d(Direction2d::X, dt, 60.0));
+        obj2.apply(move2d(Direction2d::X, dt, 60.0));
+        let collision = Collision2d { obj1: obj, obj2 };
+
+        if collision.collider() {
+            let u = elastic_collision::calculate(obj.velocity, obj.mass, obj2.mass, obj2.velocity);
+
+            let u2 = elastic_collision::calculate(obj2.velocity, obj2.mass, obj.mass, obj.velocity);
+            obj2.velocity = u2;
+            obj.velocity = u;
+            obj.acceleration = u;
+            obj2.acceleration = u2;
+        }
+
         canvas.filled_circle(
             obj.vec.x as i16,
             obj.vec.y as i16,
             obj.radius as i16,
             Color::RGB(0, 0, 255),
+        )?;
+
+        canvas.filled_circle(
+            obj2.vec.x as i16,
+            obj.vec.y as i16,
+            obj.radius as i16,
+            Color::BLUE,
         )?;
         canvas.present();
 
